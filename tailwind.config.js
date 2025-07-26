e81d251d3c1b10743afc086afd8f7d6ad8d31f64
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-07-26 18:22:09 +0200

    erste Version mit Tailwind CSS design

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Sucht in deiner index.html im Stammverzeichnis
+    // "./src/**/*.{html,js,ts}", // Beispiel: Falls du HTML, JS oder TS in einem 'src'-Ordner hast (optional)
+    // Füge hier weitere Pfade hinzu, wenn du mehr HTML-Dateien oder JS/TS hast, die HTML generieren
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
