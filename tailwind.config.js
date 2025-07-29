21bb33b063d0ab3f8658c5738acfa5b3e425948b
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-07-29 21:41:58 +0200

    resr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html", // Sucht in deiner index.html im Stammverzeichnis
+    "./index.html" ,"./andereVarianteDesign.html", "voktestmitids" // Sucht in deiner index.html im Stammverzeichnis
     // "./src/**/*.{html,js,ts}", // Beispiel: Falls du HTML, JS oder TS in einem 'src'-Ordner hast (optional)
     // Füge hier weitere Pfade hinzu, wenn du mehr HTML-Dateien oder JS/TS hast, die HTML generieren
   ],
