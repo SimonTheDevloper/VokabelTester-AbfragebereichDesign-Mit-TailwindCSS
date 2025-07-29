c44591d7d05e88b103ca529d3c4aebd569c45066
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-07-29 21:42:10 +0200

    resr

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// .prettierrc.js
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  // Optional: Weitere Prettier-Optionen hinzufügen
+  // z.B. printWidth: 100,
+  // tabWidth: 2,
+  // semi: false,
+  // singleQuote: true,
+};
